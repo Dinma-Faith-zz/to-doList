eb3dfc140e2f2b8f1e372e7ef23adbba919d6b73
Dinma-Faith <phaytep@gmail.com>
2022-04-28 16:58:17 +0100

    Create a structure for todo-List

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import _ from 'lodash';
+import './style.css';
+
+const displayPage = document.querySelector('.displayPage');
+
+const todoList = [
+    { id: 0, description: 'Wash the dishes', completed: false },
+    { id: 1, description: 'Go to the gym', completed: true },
+    { id: 2,description: 'Eat breakfast', completed: false },
+    { id: 3, description: 'Go for a walk', completed: true },
+  ];
+  displayPage.innerHTML = '';
+  todoList.map((todo) => {
+      todoList.sort((x, y) => x - y);
+      displayPage.innerHTML += `
+      <div class="tasktodo" id= ${todo.id}> <input type="checkbox" ${todo.completed ? 'checked' : 'unchecked'}>
+      <span class="description">${todo.description}</span><div class="fa-solid fa-ellipsis-vertical fa-lg"></div>
+      </div>`;
+      return todo;
+  })
